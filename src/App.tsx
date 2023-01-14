3ccba964f8af150911780498d68e6c02103bd453
Bruno Buss <brunooliveira9895@gmail.com>
2023-01-14 23:41:53 +0000

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { Map } from './pages/Map';
 function App() {
 
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<Map />} />
